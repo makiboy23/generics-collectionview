7fff4a73e717027695a5873c08b0301034bfb288
makiboy23 <marknel.pineda23@gmail.com>
2018-07-06 07:11:15 +0800

    Initial Commit

---
+++ Generics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Generics
+//
+//  Created by Marknel Pineda on 06/07/2018.
+//  Copyright © 2018 mvpineda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
